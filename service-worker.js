40199e76b143c06744d7264bc1059e3b5233391c
rhonesegara <rhonesegara78@gmail.com>
2026-03-02 14:54:48 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "taskboard-cache-v1";
+const FILES_TO_CACHE = [
+    "index.html",
+    "manifest.json",
+    "r1.png",
+    "r2.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
+});
